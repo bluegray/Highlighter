92bff65c69b6b2a90437bf4fedbb8950f87169b6
Francois du Toit <francois@cognician.com>
2013-09-07 22:26:08 +0200

    Add custom regex and settings file. Rename to Highlighter

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-#Highlight Mixed Whitespace
+#Highlighter
 
 A plugin for [Sublime Text](http://www.sublimetext.com) to highlight mixed
-tabs and spaces.
+tabs and spaces, some unicode characters and trailing space.
+
+You can also add a custom regex for characters to highlight.
+
+See the default settings file if you want to customise
+
+This plugin used to be called *Highlight Mixed Whitespace*, now renamed because it does a little more now.
 
 Shamelessly ripped from https://github.com/SublimeText/TrailingSpaces and modified.
 
-![Highlight Mixed Whitespace Screenshot](https://github.com/bluegray/Highlight-Mixed-Whitespace/raw/master/images/mixed-whitespace.png "Highlight Mixed Whitespace Screenshot")
+![Highlighter Screenshot](https://github.com/bluegray/Highlight-Mixed-Whitespace/raw/master/images/mixed-whitespace.png "Highlighter Screenshot")
--- highlighter.py
+++ highlighter.py
@@ -7,11 +7,12 @@ import sublime_plugin
 DEFAULT_MAX_FILE_SIZE = 1048576
 DEFAULT_COLOR_SCOPE_NAME = "invalid"
 DEFAULT_IS_ENABLED = True
+DEFAULT_REGEX = '(\t+ +)|( +\t+)|[\u2026\u2018\u2019\u201c\u201d\u2013\u2014]|[\t ]+$'
 
 # Set whether the plugin is on or off.
-hmw_settings = sublime.load_settings('highlight_mixed_whitespace.sublime-settings')
-hmw_enabled = bool(hmw_settings.get('hmw_enabled', DEFAULT_IS_ENABLED))
-
+highlighter_settings = sublime.load_settings('highlighter.sublime-settings')
+hmw_enabled = bool(highlighter_settings.get('highlighter_enabled', DEFAULT_IS_ENABLED))
+hmw_regex = highlighter_settings.get('highlighter_regex', DEFAULT_REGEX)
 
 # Determine if the view is a find results view.
 def is_find_results(view):
@@ -19,25 +20,25 @@ def is_find_results(view):
         "Find Results" in view.settings().get('syntax')
 
 
-# Return an array of regions matching mixed whitespace.
-def find_mixed_whitespace(view):
-    return view.find_all('(\t+ +)|( +\t+)')
+# Return an array of regions matching regex.
+def find_regexes(view):
+    return view.find_all(hmw_regex)
 
 
-# Highlight mixed whitspace.
-def highlight_mixed_whitespace(view):
-    max_size = hmw_settings.get('highlight_mixed_whitespace_max_file_size',
+# Highlight regex matches.
+def highlighter(view):
+    max_size = highlighter_settings.get('highlighter_max_file_size',
                                 DEFAULT_MAX_FILE_SIZE)
-    color_scope_name = hmw_settings.get('highlight_mixed_whitespace_scope_name',
+    color_scope_name = highlighter_settings.get('highlighter_scope_name',
                                         DEFAULT_COLOR_SCOPE_NAME)
     if view.size() <= max_size and not is_find_results(view):
-        regions = find_mixed_whitespace(view)
-        view.add_regions('MixedWhitespaceHighlightListener', regions,
+        regions = find_regexes(view)
+        view.add_regions('HighlighterListener', regions,
                          color_scope_name, "", sublime.DRAW_EMPTY)
 
 
 # Highlight matching regions.
-class MixedWhitespaceHighlightListener(sublime_plugin.EventListener):
+class HighlighterListener(sublime_plugin.EventListener):
     def __init__(self):
         self.pending = 0
 
@@ -50,12 +51,12 @@ class MixedWhitespaceHighlightListener(sublime_plugin.EventListener):
         self.pending = self.pending - 1
         if self.pending > 0:
             return
-        highlight_mixed_whitespace(view)
+        highlighter(view)
 
     def on_activated(self, view):
         if hmw_enabled:
-            highlight_mixed_whitespace(view)
+            highlighter(view)
 
     def on_load(self, view):
         if hmw_enabled:
-            highlight_mixed_whitespace(view)
+            highlighter(view)
