25d0205c91f4210a8f94e208aee1e0a66424b1e7
Francois du Toit <francois@cognician.com>
2013-09-28 13:40:37 +0200

    Cleanup

--- highlighter.py
+++ highlighter.py
@@ -10,21 +10,23 @@ DEFAULT_IS_ENABLED = True
 DEFAULT_REGEX = '(\t+ +)|( +\t+)|[\u2026\u2018\u2019\u201c\u201d\u2013\u2014]|[\t ]+$'
 DEFAULT_DELAY = 3000
 
+
+class Preferences:
+    def load(self, settings):
+        self.enabled = bool(settings.get('highlighter_enabled', DEFAULT_IS_ENABLED))
+        self.regex = settings.get('highlighter_regex', DEFAULT_REGEX)
+        self.max_size = settings.get('highlighter_max_file_size', DEFAULT_MAX_FILE_SIZE)
+        self.color_scope_name = settings.get('highlighter_scope_name', DEFAULT_COLOR_SCOPE_NAME)
+        self.delay = settings.get('highlighter_delay', DEFAULT_DELAY)
+
+Pref = Preferences()
+
+
 def plugin_loaded():
-    global Pref
     settings = sublime.load_settings('highlighter.sublime-settings')
-    class Preferences:
-        def load(self):
-            Pref.enabled          = bool(settings.get('highlighter_enabled', DEFAULT_IS_ENABLED))
-            Pref.regex            = settings.get('highlighter_regex', DEFAULT_REGEX)
-            Pref.max_size         = settings.get('highlighter_max_file_size', DEFAULT_MAX_FILE_SIZE)
-            Pref.color_scope_name = settings.get('highlighter_scope_name', DEFAULT_COLOR_SCOPE_NAME)
-            Pref.delay            = settings.get('highlighter_delay', DEFAULT_DELAY)
+    Pref.load(settings)
+    settings.add_on_change('reload', lambda: Pref.load(settings))
 
-    Pref = Preferences()
-    Pref.load()
-
-    settings.add_on_change('reload', lambda: Pref.load())
 
 # Determine if the view is a find results view.
 def is_find_results(view):
